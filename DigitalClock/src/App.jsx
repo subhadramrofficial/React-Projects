369722976d8de65b5ec02c6e12bfb417b4323c24
subhadramrofficial <subhadramr.official@gmail.com>
2026-09-15 00:16:38 +0530

    Add To-do list and Digital Clock

---
+++ DigitalClock/src/App.jsx
@@ -0,0 +1,9 @@
+import DigitalClock from './DigitalClock.jsx'
+
+function App() {
+  return (
+    <div><DigitalClock/></div>
+  )
+}
+
+export default App
---
+++ To-do/src/App.jsx
@@ -0,0 +1,9 @@
+import ToDoList from './ToDolist.jsx'
+
+function App () {
+  return (
+    <div><ToDoList/></div>
+  )
+}
+
+export default App
